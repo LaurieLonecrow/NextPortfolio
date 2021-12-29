cea6e9b23432ae2bfd8b185cc8843be53c462d4e
Laurie Lonecrow <laurieteresa@gmail.com>
2021-12-29 09:46:37 -0500

    fix: update Timeline detail

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,6 +4,7 @@ export const projects = [
     description: "A Single Page, to rule them all",
     image: 'https://res.cloudinary.com/lonecrow/image/upload/v1638658368/IMG_0017_uxlhkn.jpg',
     visit: 'https://elysia-honey.vercel.app/',
+    source: 'https://github.com/LaurieLonecrow/Elysia-Honey',
     id: 1,
   },
   {
@@ -11,6 +12,7 @@ export const projects = [
     description: "Feel the MERN",
     image: 'https://res.cloudinary.com/lonecrow/image/upload/v1636917959/stage_dyjbzz.jpg',
     visit: 'https://github.com/LaurieLonecrow/mixed-tape-react',
+    source: 'https://github.com/LaurieLonecrow/mixed-tape-react',
     id: 2,
   },
   {
@@ -18,6 +20,7 @@ export const projects = [
     description: "Shall wE-Commerce",
     image: 'https://res.cloudinary.com/lonecrow/image/upload/v1640736051/BlkTriCode_qoeapr.png',
     visit: 'https://stormy-springs-10135.herokuapp.com/',
+    source: 'https://github.com/LaurieLonecrow/BlkTri',
     id: 3,
   },
   {
@@ -25,14 +28,15 @@ export const projects = [
     description: "Back to Basics",
     image: 'https://res.cloudinary.com/lonecrow/image/upload/v1640736235/blackJackCode_rsdyqx.png',
     visit: 'https://laurielonecrow.github.io/Blackjack/',
+    source: 'https://github.com/LaurieLonecrow/Blackjack',
     id: 4,
   },
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: `Started my journey`, },
-  { year: 2018, text: `Began work as a freelance developer`, },
-  { year: 2019, text: `Built Greasy-Rider, motorcycle parts marketplace`, },
-  { year: 2020, text: `Shared my projects with the world`, },
+  { year: 2017, text: `Started my journey in software engineering`, },
+  { year: 2018, text: `Began work as a freelance web developer`, },
+  { year: 2019, text: `Built my first full-stack app, Greasy-Rider: a motorcycle parts marketplace`, },
+  { year: 2020, text: `Took on more projects and taught myself new skills`, },
   { year: 2021, text: `Back on my bullsh*t, freelancing and looking for a steady gig`, },
 ];
